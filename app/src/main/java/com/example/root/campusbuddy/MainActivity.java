85e2e5ff604f8361213e50c772e43c564100f13b
raghav <raghav.chawla63@gmail.com>
2015-05-13 02:35:45 +0530

    added the google maps (though not limited it only to IITR)

--- app/src/main/java/com/example/root/campusbuddy/MainActivity.java
+++ app/src/main/java/com/example/root/campusbuddy/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.root.campusbuddy;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button mapButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       mapButt = (Button) findViewById(R.id.mapBut);
+
+        mapButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(MainActivity.this, map.class);
+                startActivity(mapIntent);
+            }
+        });
+
+
     }
 
     @Override
@@ -36,4 +52,8 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
