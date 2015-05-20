7a873ed94bb774782acb8c3404c6e656ee0cb4f7
Manthan-R-Sheth <manthanrsheth96@gmail.com>
2015-05-21 00:00:39 +0530

    Navigation Drawer added but with errors

--- app/src/main/java/com/example/root/campusbuddy/timetable3.java
+++ app/src/main/java/com/example/root/campusbuddy/timetable3.java
@@ -13,17 +13,22 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.alamkanak.weekview.DateTimeInterpreter;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
@@ -39,8 +44,7 @@ import java.util.Locale;
  * Created by Raquib-ul-Alam Kanak on 7/21/2014.
  * Website: http://alamkanak.github.io/
  */
-public class timetable3 extends ActionBarActivity implements WeekView.MonthChangeListener,
-        WeekView.EventClickListener, WeekView.EventLongPressListener ,delete_edit_choose.AlertPositiveListener {
+public class timetable3 extends AppCompatActivity implements WeekView.MonthChangeListener, WeekView.EventClickListener, WeekView.EventLongPressListener ,delete_edit_choose.AlertPositiveListener,Navigation_Drawer_Fragment.NavigationDrawerCallbacks {
 
     SQLiteDatabase db;
     Cursor cr;
@@ -55,6 +59,8 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
     private static final int TYPE_WEEK_VIEW = 3;
     private int mWeekViewType = TYPE_THREE_DAY_VIEW;
     private WeekView mWeekView;
+    private Navigation_Drawer_Fragment mNavigationDrawerFragment;
+
     //String delvalue=null;
     SharedPreferences pref;
         int position=0;
@@ -65,10 +71,31 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timetable);
-
-
         fa = this;
+
+
+        try{
+        setContentView(R.layout.activity_timetable);
+         mNavigationDrawerFragment = (Navigation_Drawer_Fragment)(getSupportFragmentManager().findFragmentById(R.id.navigation_drawer));
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this,e.toString()+"1",Toast.LENGTH_SHORT).show();
+        }
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        try{
+
+            mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));}
+        catch(Exception e)
+        {
+            Toast.makeText(this,e.toString()+"2",Toast.LENGTH_SHORT).show();
+        }
+
 
         pref = this.getSharedPreferences(
                 "com.example.appss", Context.MODE_PRIVATE);
@@ -117,9 +144,9 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
         }
 */
 
-
+        try{
         // Get a reference for the week view in the layout.
-        mWeekView = (WeekView) findViewById(R.id.weekView);
+        mWeekView = (WeekView) fa.findViewById(R.id.weekView);
 
         // Show a toast message about the touched event.
         mWeekView.setOnEventClickListener(this);
@@ -133,7 +160,12 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
 
         // Set up a date time interpreter to interpret how the date and time will be formatted in
         // the week view. This is optional.
-        setupDateTimeInterpreter(false);
+        setupDateTimeInterpreter(false);}
+
+        catch(Exception e)
+        {
+            Toast.makeText(this,e.toString()+"3",Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -141,6 +173,19 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_tt, menu);
+//        try{
+//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+////            // Only show items in the action bar relevant to this screen
+////            // if the drawer is not showing. Otherwise, let the drawer
+////            // decide what to show in the action bar.
+//            getMenuInflater().inflate(R.menu.menu_tt, menu);
+//            restoreActionBar();
+//            return true;
+//        }}
+//        catch (Exception e)
+//        {
+//            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+//        }
         return true;
     }
 
@@ -148,54 +193,56 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         setupDateTimeInterpreter(id == R.id.action_week_view);
-        switch (id){
-            case R.id.action_today:
-                mWeekView.goToToday();
-                return true;
-            case R.id.action_day_view:
-                if (mWeekViewType != TYPE_DAY_VIEW) {
-                    item.setChecked(!item.isChecked());
-                    mWeekViewType = TYPE_DAY_VIEW;
-                    mWeekView.setNumberOfVisibleDays(1);
-
-                    // Lets change some dimensions to best fit the view.
-                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
-                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-                }
-                return true;
-            case R.id.action_three_day_view:
-                if (mWeekViewType != TYPE_THREE_DAY_VIEW) {
-                    item.setChecked(!item.isChecked());
-                    mWeekViewType = TYPE_THREE_DAY_VIEW;
-                    mWeekView.setNumberOfVisibleDays(3);
-
-                    // Lets change some dimensions to best fit the view.
-                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
-                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-                }
-                return true;
-            case R.id.action_week_view:
-                if (mWeekViewType != TYPE_WEEK_VIEW) {
-                    item.setChecked(!item.isChecked());
-                    mWeekViewType = TYPE_WEEK_VIEW;
-                    mWeekView.setNumberOfVisibleDays(7);
-
-                    // Lets change some dimensions to best fit the view.
-                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics()));
-                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
-                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
-                }
-                return true;
-
-            case R.id.action_new_event:
-
-                Intent newEventIntent = new Intent(timetable3.this, NewEvent.class);
-                startActivity(newEventIntent);
-
-
-        }
+//        switch (id){
+//            case R.id.action_today:
+//                mWeekView.goToToday();
+//                return true;
+//            case R.id.action_day_view:
+//                if (mWeekViewType != TYPE_DAY_VIEW) {
+//                    item.setChecked(!item.isChecked());
+//                    mWeekViewType = TYPE_DAY_VIEW;
+//                    mWeekView.setNumberOfVisibleDays(1);
+//
+//                    // Lets change some dimensions to best fit the view.
+//                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+//                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+//                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+//                }
+//                return true;
+//            case R.id.action_three_day_view:
+//                if (mWeekViewType != TYPE_THREE_DAY_VIEW) {
+//                    item.setChecked(!item.isChecked());
+//                    mWeekViewType = TYPE_THREE_DAY_VIEW;
+//                    mWeekView.setNumberOfVisibleDays(3);
+//
+//                    // Lets change some dimensions to best fit the view.
+//                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+//                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+//                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+//                }
+//                return true;
+//            case R.id.action_week_view:
+//                if (mWeekViewType != TYPE_WEEK_VIEW) {
+//                    item.setChecked(!item.isChecked());
+//                    mWeekViewType = TYPE_WEEK_VIEW;
+//                    mWeekView.setNumberOfVisibleDays(7);
+//
+//                    // Lets change some dimensions to best fit the view.
+//                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics()));
+//                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+//                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+//                }
+//                return true;
+//
+//            case R.id.action_new_event:
+//
+//                Intent newEventIntent = new Intent(timetable3.this, NewEvent.class);
+//                startActivity(newEventIntent);
+//                return true;
+//
+//
+//
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -369,52 +416,6 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
             dialog.show(getFragmentManager(), "ChooseDialogFragment");
 
 
-//        final Dialog dialog = new Dialog(timetable3.this);
-//        dialog.setContentView(R.layout.activity_deleteand_edit_events2);
-//        dialog.setTitle("Choose action...");
-//
-//
-//        TextView choose = (TextView) dialog.findViewById(R.id.choose_action);
-//        TextView edit = (TextView) dialog.findViewById(R.id.text_edit);
-//        TextView del = (TextView) dialog.findViewById(R.id.text_delete);
-//
-//        choose.setVisibility(View.INVISIBLE);
-//
-//
-//        // if button is clicked, close the custom dialog
-//        edit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SharedPreferences.Editor editor = pref.edit();
-//                editor.putInt("DELETE_OR_EDIT", 1);
-//                editor.commit();
-//
-//                //editcounter++;
-//                Intent ne=new Intent(timetable3.this,NewEvent.class);
-//                ne.putExtra("value for editing", longClickedID );
-//                startActivity(ne);
-//
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        del.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            try {
-//                    db.delete(CalendarDB.CalendarEntry.TABLE_NAME, CalendarDB.CalendarEntry.COLUMN_NAME_ID + "=" + longClickedID, null);
-//                } catch (Exception e) {
-//                    Toast.makeText(timetable3.this, e.toString(), Toast.LENGTH_LONG).show();
-//                }
-//                dialog.dismiss();
-//                finish();
-//                startActivity(getIntent());
-//
-//            }
-//        });
-//
-//        dialog.show();
     }
 
         @Override
@@ -444,6 +445,141 @@ public class timetable3 extends ActionBarActivity implements WeekView.MonthChang
 
             }
                 }
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+
+    /*
+     * Used to store the last screen title. For use in {@link restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.weekView, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = "Today";
+                mWeekView.goToToday();
+                break;
+            case 2:
+                mTitle = "3-day view";
+                if (mWeekViewType != TYPE_THREE_DAY_VIEW) {
+                   // item.setChecked(!item.isChecked());
+                    mWeekViewType = TYPE_THREE_DAY_VIEW;
+                    mWeekView.setNumberOfVisibleDays(3);
+
+                    // Lets change some dimensions to best fit the view.
+                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                }
+                break;
+            case 3:
+                mTitle = "7-day view";
+                if (mWeekViewType != TYPE_WEEK_VIEW) {
+                   // item.setChecked(!item.isChecked());
+                    mWeekViewType = TYPE_WEEK_VIEW;
+                    mWeekView.setNumberOfVisibleDays(7);
+
+                    // Lets change some dimensions to best fit the view.
+                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics()));
+                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+                }
+                break;
+            case 4:
+                Intent newEventIntent = new Intent(timetable3.this, NewEvent.class);
+                startActivity(newEventIntent);
+
+        }
+    }
+
+//    public void restoreActionBar() {
+//        ActionBar actionBar =getSupportActionBar();
+//
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+//        actionBar.setDisplayShowTitleEnabled(true);
+//        actionBar.setTitle(mTitle);
+//    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+//            // Only show items in the action bar relevant to this screen
+//            // if the drawer is not showing. Otherwise, let the drawer
+//            // decide what to show in the action bar.
+//            getMenuInflater().inflate(R.menu.main, menu);
+//            restoreActionBar();
+//            return true;
+//        }
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((timetable3) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
 }
 
 
