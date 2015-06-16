5daddd637ea88f0f645e20b1fd3cdcf8576a3e27
raghav <raghav.chawla63@gmail.com>
2015-06-16 14:27:36 +0530

    FB sdk set

--- app/src/main/java/mobileDevelopment/com/root/campusbuddy/MainActivity.java
+++ app/src/main/java/mobileDevelopment/com/root/campusbuddy/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends Activity {
 
     Button mapButt1, mapButt2, tnButt2,tdbtt1;
@@ -15,6 +17,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         mapButt1 = (Button) findViewById(R.id.mapBut1);
         mapButt2 = (Button) findViewById(R.id.mapBut2);
