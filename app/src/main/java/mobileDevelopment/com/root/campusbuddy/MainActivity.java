edd1e8b727ee949b31589285911b604290bd40f9
raghav <raghav.chawla63@gmail.com>
2015-08-05 16:22:46 +0530

    UI improved after proper icons and designs

--- app/src/main/java/mobileDevelopment/com/root/campusbuddy/MainActivity.java
+++ app/src/main/java/mobileDevelopment/com/root/campusbuddy/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.facebook.AccessToken;
@@ -30,8 +31,9 @@ import com.facebook.rebound.SpringSystem;
 
 public class MainActivity extends AppCompatActivity {
 
-   ImageButton mapButt1, mapButt2, tnButt2,tdbtt1, fbbtt1;
+   ImageView mapButt1, mapButt2, tnButt2,tdbtt1, fbbtt1;
     private FloatingActionButton mActionButton;
+    ProgressBar p;
 //    SharedPreferences prefsforfb;
 
    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -77,10 +79,10 @@ public class MainActivity extends AppCompatActivity {
 //        SharedPreferences.Editor editor = prefsforfb.edit();
 //        editor.putInt("No of times ", 0);
 //        editor.commit();
-        mapButt1 = (ImageButton) findViewById(R.id.mapBut1);
-        tnButt2 = (ImageButton) findViewById(R.id.tnBut2);
-        tdbtt1=(ImageButton)findViewById(R.id.tdbtn);
-        fbbtt1=(ImageButton)findViewById(R.id.fbbtn);
+        mapButt1 = (ImageView) findViewById(R.id.mapBut1);
+        tnButt2 = (ImageView) findViewById(R.id.tnBut2);
+        tdbtt1=(ImageView)findViewById(R.id.tdbtn);
+        fbbtt1=(ImageView)findViewById(R.id.fbbtn);
 
 //        SpringSystem springSystem = SpringSystem.create();
 //// Add a spring to the system.
