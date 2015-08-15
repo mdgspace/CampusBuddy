beada5d9c376ec74bf4ccf2da7acf4acea43ad99
ankurgarg101 <ankurgarg101@gmail.com>
2015-08-15 23:08:41 +0530

    Adds check for invalid contact_nos and email_ids

--- app/src/main/java/mobileDevelopment/com/root/campusbuddy/MyRecyclerAdapter_departmentcontacts.java
+++ app/src/main/java/mobileDevelopment/com/root/campusbuddy/MyRecyclerAdapter_departmentcontacts.java
@@ -1,11 +1,13 @@
 package mobileDevelopment.com.root.campusbuddy;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +18,12 @@ import java.util.List;
 public class MyRecyclerAdapter_departmentcontacts extends RecyclerView.Adapter<ContactViewHolderdept>
 {
     public List<Contact> contacts;
-    public MyRecyclerAdapter_departmentcontacts(List<Contact> contacts)
+    public Context mContext;
+    public MyRecyclerAdapter_departmentcontacts(List<Contact> contacts, Context context)
     {
         this.contacts=new ArrayList<Contact>();
         this.contacts.addAll(contacts);
+        mContext = context;
     }
 
     @Override
@@ -32,23 +36,43 @@ public class MyRecyclerAdapter_departmentcontacts extends RecyclerView.Adapter<C
     public void onBindViewHolder(ContactViewHolderdept holder, final int position) {
         Contact contact=contacts.get(position);
         holder.nameT.setText(contact.getName());
+        boolean valid_phone_number = true;
+        boolean valid_email = true;
+        if(TelephoneContacts.contactnos[position] == null || TelephoneContacts.contactnos[position].length() == 0){
+            valid_phone_number = false;
+            holder.call.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_call_black_18dp));
+        }
+        if(TelephoneContacts.emailids[position] == null || TelephoneContacts.emailids[position].length() == 0){
+            valid_email = false;
+            holder.email.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_call_black_18dp));
+        }
+        final boolean call_status = valid_phone_number;
+        final boolean email_status = valid_email;
         holder.call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + TelephoneContacts.contactnos[position]));
-                view.getContext().startActivity(intent);
+                if(call_status){
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + TelephoneContacts.contactnos[position]));
+                    view.getContext().startActivity(intent);
+                }else{
+                    Toast.makeText(mContext, "Contact Number not available", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         holder.email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setType("plain/text");
+                if(email_status){
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setType("plain/text");
 //                intent.setData(Uri.parse("www.gmail.com"));
-                intent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{TelephoneContacts.emailids[position]});
-                view.getContext().startActivity(intent);
+                    intent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{TelephoneContacts.emailids[position]});
+                    view.getContext().startActivity(intent);
+                }else{
+                    Toast.makeText(mContext, "Email Id not available", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
