4f711c2883b15353ce534fe4a442d810b3a162d9
raghav <raghav.chawla63@gmail.com>
2015-09-10 13:54:56 +0530

    Deleted unnecesaary layout files

--- app/src/main/java/in/co/mdg/campusbuddy/MainActivity.java
+++ app/src/main/java/in/co/mdg/campusbuddy/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main);
 
 
        SharedPreferences getPrefs = PreferenceManager
