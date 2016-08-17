2976470acab6638ae76d7cd4a37e7a955e0e57fb
csoni111 <csoni111@gmail.com>
2016-08-17 17:01:11 +0530

    Rebased feature-fcm on top of design-updates, for latest gradle dependencies.
    -Merge conflicts resolved.
    -Added google-services.json to app folder.
    -Added plugin for google-services to build.gradle.
    
    Signed-off-by: csoni111 <csoni111@gmail.com>

---
+++ app/src/main/java/in/co/mdg/campusBuddy/FirebaseMessagingService.java
@@ -0,0 +1,39 @@
+package in.co.mdg.campusBuddy;
+
+/**
+ * Created by mohit on 17/8/16.
+ */
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by mohit on 11/8/16.
+ */
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        showNotification(remoteMessage.getData().get("message"));
+    }
+
+    private void showNotification(String message) {
+        Intent i = new Intent(this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("test")
+                .setContentText(message)
+                .setSmallIcon(R.drawable.facebook_icon)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(0,builder.build());
+
+    }
+}
